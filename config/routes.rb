7b33b4f53473ee8a7505d4078a092fdeba682d6f
Ken Carbullido <kdcarbullido@gmail.com>
2018-03-16 18:51:25 +0000

    Initial CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :posts
+  
+#  get "welcome/index"
+#  get "welcome/about"
+
+  get 'about' => 'welcome#about'
   
   root 'welcome#index'
 
